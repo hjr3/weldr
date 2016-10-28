62a10ed5c7271580aa9cd2c3b2809540a0b621df
Herman J. Radtke III <herman@hermanradtke.com>
2016-10-27 21:33:24 -0400

    Use only one buffer per pipe

--- src/pipe.rs
+++ src/pipe.rs
@@ -21,13 +21,9 @@ pub struct Pipe {
     server: TcpStream,
     state: ConnectionState,
 
-    /// The buffer from the client to send to the server
-    send_buf: [u8; 1024],
-    send_num_bytes: usize,
-
-    /// The buffer from the server to send to the client
-    recv_buf: [u8; 1024],
-    recv_num_bytes: usize,
+    /// A buffer to keep track of the data betewen the client and server
+    buf: [u8; 1024],
+    num_bytes: usize,
 }
 
 impl Pipe {
@@ -38,10 +34,8 @@ impl Pipe {
             client: client,
             server: server,
             state: ConnectionState::ClientReading,
-            send_buf: [0u8; 1024],
-            send_num_bytes: 0,
-            recv_buf: [0u8; 1024],
-            recv_num_bytes: 0,
+            buf: [0u8; 1024],
+            num_bytes: 0,
         }
     }
 }
@@ -59,60 +53,60 @@ impl Future for Pipe {
                 ConnectionState::ClientReading => {
                     trace!("Reading from {}", self.client_addr);
 
-                    let bytes = try_ready!(self.client.try_read(&mut self.send_buf));
+                    let bytes = try_ready!(self.client.try_read(&mut self.buf));
                     trace!("Read {} bytes from {}", bytes, self.client_addr);
 
                     if bytes == 0 {
                         self.state = ConnectionState::ServerWriting;
                         trace!("State switched to {:?}", self.state);
                     } else {
-                        self.send_num_bytes = bytes;
+                        self.num_bytes = bytes;
                     }
                 }
 
                 ConnectionState::ServerWriting => {
                     trace!("Writing to {}", self.server_addr);
 
-                    let bytes = try_ready!(self.server.try_write(&mut self.send_buf[0..self.send_num_bytes]));
+                    let bytes = try_ready!(self.server.try_write(&mut self.buf[0..self.num_bytes]));
                     trace!("Wrote {} bytes to {}", bytes, self.server_addr);
 
                     if bytes == 0 {
-                        self.send_num_bytes = 0;
+                        self.num_bytes = 0;
                         self.state = ConnectionState::ServerReading;
                         trace!("State switched to {:?}", self.state);
                     } else {
-                        self.send_num_bytes -= bytes;
+                        self.num_bytes -= bytes;
                     }
                 }
 
                 ConnectionState::ServerReading => {
                     trace!("Reading from {}", self.server_addr);
 
-                    let bytes = try_ready!(self.server.try_read(&mut self.recv_buf));
+                    let bytes = try_ready!(self.server.try_read(&mut self.buf));
                     trace!("Read {} bytes from {}", bytes, self.server_addr);
 
                     if bytes == 0 {
                         self.state = ConnectionState::ClientWriting;
                         trace!("State switched to {:?}", self.state);
                     } else {
-                        self.recv_num_bytes = bytes;
+                        self.num_bytes = bytes;
                     }
                 }
 
                 ConnectionState::ClientWriting => {
                     trace!("Writing to {}", self.client_addr);
 
-                    let bytes = try_ready!(self.client.try_write(&mut self.recv_buf[0..self.recv_num_bytes]));
+                    let bytes = try_ready!(self.client.try_write(&mut self.buf[0..self.num_bytes]));
                     trace!("Wrote {} bytes to {}", bytes, self.client_addr);
 
                     if bytes == 0 {
-                        self.recv_num_bytes = 0;
+                        self.num_bytes = 0;
                         self.state = ConnectionState::ClientReading;
                         trace!("State switched to {:?}", self.state);
                         trace!("Bailing out of loop");
                         break;
                     } else {
-                        self.recv_num_bytes -= bytes;
+                        self.num_bytes -= bytes;
                     }
                 }
             }
