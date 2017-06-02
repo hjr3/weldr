2edeac3bfdfe14743aa26ac524027001d5c0f5dd
Herman J. Radtke III <herman@hermanradtke.com>
2017-06-02 07:38:43 -0700

    Listen on 0.0.0.0 instead of localhost by default

--- src/weldr.rs
+++ src/weldr.rs
@@ -24,12 +24,12 @@ fn main() {
               .long("admin-ip")
               .value_name("admin-ip")
               .takes_value(true)
-              .help("administrative ip and port used to issue commands to cluster. defaults to 127.0.0.1:8687"))
+              .help("administrative ip and port used to issue commands to cluster. defaults to 0.0.0.0:8687"))
          .arg(Arg::with_name("ip")
               .long("ip")
               .value_name("ip")
               .takes_value(true)
-              .help("listening ip and port for cluster. defaults to 127.0.0.1:8080"))
+              .help("listening ip and port for cluster. defaults to 0.0.0.0:8080"))
          .subcommand(SubCommand::with_name("worker")
             .about("start a worker")
             .arg(Arg::with_name("id")
@@ -47,7 +47,7 @@ fn main() {
     let internal_addr = "127.0.0.1:4000";
     let internal_addr = internal_addr.parse::<SocketAddr>().expect("Failed to parse addr");
 
-    let ip = matches.value_of("worker").unwrap_or("127.0.0.1:8080");
+    let ip = matches.value_of("worker").unwrap_or("0.0.0.0:8080");
     let ip = ip.parse::<SocketAddr>().unwrap();
 
     let pool = Pool::default();
@@ -66,7 +66,7 @@ fn main() {
 
         let health = BackendHealth::new();
 
-        let admin_ip = matches.value_of("worker").unwrap_or("127.0.0.1:8687");
+        let admin_ip = matches.value_of("worker").unwrap_or("0.0.0.0:8687");
         let admin_ip = admin_ip.parse::<SocketAddr>().unwrap();
         weldr::mgmt::run(admin_ip, pool, core, manager.clone(), &conf, health.clone()).expect("Failed to start server");
     }
